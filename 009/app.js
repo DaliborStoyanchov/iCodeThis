589963f4c10fffef9d6db19accc0f22798f0d751
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2024-07-04 19:59:13 +0200

    009 styles and functionality, splide

--- 009/app.js
+++ 009/app.js
@@ -1,2 +1,17 @@
-var splide = new Splide(".splide");
+let splide = new Splide(".splide");
+
 splide.mount();
+
+const addContactsBtns = [...document.querySelectorAll(".profile > button")];
+
+addContactsBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (btn.textContent === "Added") {
+      btn.classList.remove("added");
+      btn.textContent = "Add contact";
+    } else {
+      btn.classList.add("added");
+      btn.textContent = "Added";
+    }
+  });
+});
