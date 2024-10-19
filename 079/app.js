898605fb0d89aa85a0b873c9538fabede0cca912
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2024-10-19 09:15:55 +0200

    079 Dashboard UI

---
+++ 079/app.js
@@ -0,0 +1,37 @@
+const usersChart = new Chart(document.querySelector(".users"), {
+  type: "line",
+  data: {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May"],
+    datasets: [
+      {
+        label: "Users",
+        data: [10, 5, 15, 2, 8],
+        fill: true,
+        borderColor: "#9c75ff",
+        backgroundColor: "#f3f3ff",
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+  },
+});
+
+const newUsersChart = new Chart(document.querySelector(".new-users"), {
+  type: "line",
+  data: {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May"],
+    datasets: [
+      {
+        label: "New users",
+        data: [0, 10, 3, 12, 6],
+        fill: true,
+        borderColor: "#9c75ff",
+        backgroundColor: "#f3f3ff",
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+  },
+});
