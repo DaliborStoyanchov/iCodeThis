2f81f1e64437d8a59e088d9da54dbc91f7bf2169
DaliborStoyanchov <119995923+DaliborStoyanchov@users.noreply.github.com>
2024-10-31 13:35:57 +0100

    090 Tabbed UI

---
+++ 090/app.js
@@ -0,0 +1,30 @@
+const SECTIONS = [...document.querySelectorAll("section")];
+const TABS = [...document.querySelectorAll("li button")];
+
+// console.log(TABS, SECTIONS);
+
+TABS.forEach((tab) => {
+  tab.addEventListener("click", (e) => {
+    SECTIONS.forEach((section) => (section.style.display = "none"));
+
+    TABS.forEach((t) => {
+      t.classList.remove("active");
+    });
+
+    e.target.classList.add("active");
+
+    switch (e.target.textContent) {
+      case "Details":
+        SECTIONS[0].style.display = "block";
+        break;
+      case "Tutorials":
+        SECTIONS[1].style.display = "block";
+        break;
+      case "Code snippets":
+        SECTIONS[2].style.display = "block";
+        break;
+      default:
+        SECTIONS[0].style.display = "block";
+    }
+  });
+});
